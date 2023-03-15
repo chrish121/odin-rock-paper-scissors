37a7fb8fb4c60eece5f31d8e0388a9b20bc82033
Chris <123729087+chrish121@users.noreply.github.com>
2023-03-15 22:10:29 +0800

    Add computer choice script

---
+++ javascript.js
@@ -0,0 +1,7 @@
+function getComputerChoice() {
+    let choices = ["rock", "paper", "scissors"];
+    let randomChoice = choices[(Math.floor(Math.random() * (choices.length)))];
+    return(randomChoice);
+}
+
+console.log(getComputerChoice());
