e4ebbe0209852969c5249923c9dea2e7d65c0955
Chris <123729087+chrish121@users.noreply.github.com>
2023-03-15 23:37:01 +0800

    Add script for player choice and a single round of the game

--- javascript.js
+++ javascript.js
@@ -1,7 +1,43 @@
+let playerChoice = prompt("Rock, Paper, or Scissors?");
+
 function getComputerChoice() {
     let choices = ["rock", "paper", "scissors"];
     let randomChoice = choices[(Math.floor(Math.random() * (choices.length)))];
     return(randomChoice);
 }
 
-console.log(getComputerChoice());
+let playerCapital = (playerChoice.charAt(0)).toUpperCase();
+let remainingPlayer = playerChoice.substring(1);
+let playerLowercase = remainingPlayer.toLowerCase();
+let playerCase = (playerCapital + playerLowercase);
+
+let computerChoice = getComputerChoice();
+let computerCapital = (computerChoice.charAt(0)).toUpperCase();
+let remainingComputer = computerChoice.substring(1);
+let computerLowercase = remainingComputer.toLowerCase();
+let computerCase = (computerCapital + computerLowercase);
+
+let playerSelection = playerCase;
+let computerSelection = computerCase;
+
+function playRound(playerSelection, computerSelection) {
+    if ((playerSelection == "Rock" && computerSelection == "Scissors") || 
+    (playerSelection == "Scissors" && computerSelection == "Paper") ||
+    (playerSelection == "Paper" && computerSelection == "Rock")) {
+        return("You Win! " + playerSelection + " beats " + computerSelection + ".");
+    }
+
+    else if ((playerSelection == "Rock" && computerSelection == "Paper") ||
+    (playerSelection == "Scissors" && computerSelection == "Rock") ||
+    (playerSelection == "Paper" && computerSelection == "Scissors")) {
+        return("You Lose! " + computerSelection + " beats " + playerSelection + ".");
+    }
+
+    else if (playerSelection == computerSelection) {
+        return("It's a Tie! Try again.");
+    }
+    
+    else {
+        return("Invalid");
+    }
+}
