82dd5dfea6d08ff0bb740c4d5667b472a312257f
Chris <123729087+chrish121@users.noreply.github.com>
2023-04-17 07:04:02 +0800

    Add event listener for playRound function with scoring

--- javascript.js
+++ javascript.js
@@ -1,57 +1,95 @@
 let playerPoints = 0;
 let computerPoints = 0;
 
-for (let i = 0; i < 5; i++) {
+/* for (let i = 0; i < 5; i++) {
 
 let playerChoice = prompt("Rock, Paper, or Scissors?");
 if (playerChoice === null) {
     console.log("Invalid");
     break;
-}
+} */
 
 function getComputerChoice() {
-    let choices = ["rock", "paper", "scissors"];
+    let choices = ["Rock", "Paper", "Scissors"];
     let randomChoice = choices[(Math.floor(Math.random() * (choices.length)))];
     return(randomChoice);
 }
 
-let playerCapital = (playerChoice.charAt(0)).toUpperCase();
-let remainingPlayer = playerChoice.substring(1);
-let playerLowercase = remainingPlayer.toLowerCase();
-let playerCase = (playerCapital + playerLowercase);
+const buttons = document.querySelectorAll("button");
 
-let computerChoice = getComputerChoice();
-let computerCapital = (computerChoice.charAt(0)).toUpperCase();
-let remainingComputer = computerChoice.substring(1);
-let computerLowercase = remainingComputer.toLowerCase();
-let computerCase = (computerCapital + computerLowercase);
+buttons.forEach((button) => {
+    button.addEventListener("click", playRound)
+    });
 
-let playerSelection = playerCase;
-let computerSelection = computerCase;
 
-function playRound(playerSelection, computerSelection) {
-    if ((playerSelection == "Rock" && computerSelection == "Scissors") || 
-    (playerSelection == "Scissors" && computerSelection == "Paper") ||
-    (playerSelection == "Paper" && computerSelection == "Rock")) {
-        return("You Win! " + playerSelection + " beats " + computerSelection + ".");
-    }
+function playRound() {
+    var playerSelection = this.id;
+    var computerSelection = getComputerChoice();
 
-    else if ((playerSelection == "Rock" && computerSelection == "Paper") ||
-    (playerSelection == "Scissors" && computerSelection == "Rock") ||
-    (playerSelection == "Paper" && computerSelection == "Scissors")) {
-        return("You Lose! " + computerSelection + " beats " + playerSelection + ".");
-    }
+    if ((playerPoints != 5) && (computerPoints != 5)) {
+        console.log("P: " + playerSelection);
+        console.log("C: " + computerSelection);
+        function playGame(playerSelection, computerSelection) {
+            if ((playerSelection == "Rock" && computerSelection == "Scissors") || 
+            (playerSelection == "Scissors" && computerSelection == "Paper") ||
+            (playerSelection == "Paper" && computerSelection == "Rock")) {
+                return("You Win! " + playerSelection + " beats " + computerSelection + ".");
+            }
 
-    else if (playerSelection == computerSelection) {
-        return("It's a Tie!");
-    }
-    
-    else {
-        return("Invalid.");
+            else if ((playerSelection == "Rock" && computerSelection == "Paper") ||
+            (playerSelection == "Scissors" && computerSelection == "Rock") ||
+            (playerSelection == "Paper" && computerSelection == "Scissors")) {
+                return("You Lose! " + computerSelection + " beats " + playerSelection + ".");
+            }
+
+            else if (playerSelection == computerSelection) {
+                return("It's a Tie!");
+            }
+            
+            else {
+                return("Invalid.");        
+            }
+        }
+
+        function game() {    
+            if (playGame(playerSelection, computerSelection) == ("You Win! " + playerSelection + " beats " + computerSelection + ".")) {
+                let newPlayerPoints = (playerPoints += 1);
+                return (playGame(playerSelection, computerSelection) + " You get one point." + "\n" + "Player: " + newPlayerPoints + " " + "Computer: " + computerPoints);
+            }
+            else if (playGame(playerSelection, computerSelection) == ("You Lose! " + computerSelection + " beats " + playerSelection + ".")) {
+                let newComputerPoints = (computerPoints += 1);
+                return (playGame(playerSelection, computerSelection) + " Your opponent gets one point." + "\n" + "Player: " + playerPoints + " " + "Computer: " + newComputerPoints);
+            }
+            else {
+                return (playGame(playerSelection, computerSelection) + " No points." + "\n" + "Player: " + playerPoints + " " + "Computer: " + computerPoints);
+            }
+        }
+
+        console.log(game());
+
+        if ((playerPoints == 5) || (computerPoints == 5)) {
+            if (playerPoints > computerPoints) {
+                console.log("You win the tournament!");
+            }
+            else if (computerPoints > playerPoints) {
+                console.log("You lose the tournament.");
+            }
+            else if (computerPoints = playerPoints) {
+                console.log("The tournament is a tie!");
+            }
+            else {
+                console.log("There is no tournament winner.")
+            }
+        }
+    } else {
+        return;
     }
 }
 
-function game() {
+
+/* function game() {
+    var playerSelection = this.id;
+
     if (playRound(playerSelection, computerSelection) == ("You Win! " + playerSelection + " beats " + computerSelection + ".")) {
         let newPlayerPoints = (playerPoints += 1);
         return (playRound(playerSelection, computerSelection) + " You get one point." + "\n" + "Player: " + newPlayerPoints + " " + "Computer: " + computerPoints);
@@ -65,23 +103,21 @@ function game() {
     }
 }
 
-console.log(game());
-
-}
 
 function score() {
+
     if (playerPoints > computerPoints) {
-        return ("You win the tournament!");
+        return (game() + "\nYou win the tournament!");
     }
     else if (computerPoints > playerPoints) {
-        return("You lose the tournament.");
+        return(game() + "\nYou lose the tournament.");
     }
     else if (computerPoints = playerPoints) {
-        return("The tournament is a tie!");
+        return(game() + "\nThe tournament is a tie!");
     }
     else {
-        return("There is no tournament winner.")
+        return(game() + "\nThere is no tournament winner.")
     }
 }
 
-console.log(score());
+console.log(score()); */
